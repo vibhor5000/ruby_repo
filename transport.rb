8d86d9eae10c0ae4e8a1ff86713e28cc67e7acc1
Vibhor Sharma <vibhor.sharma@getdropstream.com>
2021-07-18 06:16:00 +0530

    exporting first commit

---
+++ transport.rb
@@ -0,0 +1,44 @@
+
+
+
+class Vehicles
+
+	attr_accessor :name, :year_of_manufacture, :color
+
+	def seating_capacity
+	
+
+	end
+
+	def fuel_type
+	
+
+	end
+
+	def ride_on
+
+	
+	end
+
+
+end
+
+
+class Car < Vehicles
+
+
+end	
+
+
+class Bus < Vehicles
+
+end
+
+class Aeroplane < Vehicles
+
+
+end
+
+class Boat < Vehicles
+	
+end
